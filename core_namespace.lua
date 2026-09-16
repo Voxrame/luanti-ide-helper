e57de904bcde37115b0183e252a23e0102e62a33
Alexander Chibrikin <alek13.me@gmail.com>
2026-09-16 04:42:41 +0300

    Move "Chat" into `core_namespace/registration/chat.lua`. Relates #1.

--- core_namespace.lua
+++ core_namespace.lua
@@ -318,21 +318,7 @@ function core.translate(textdomain, str, ...) end
 --   see ./core_namespace/authentication.lua
 
 -- Chat:
---- @param text string
-function core.chat_send_all(text) end
-
---- @param name string player name to send to
---- @param text string
-function core.chat_send_player(name, text) end
-
---- * Used by the server to format a chat message, based on the setting `chat_message_format`.
----   Refer to the documentation of the setting for a list of valid placeholders.
---- * Takes player name and message, and returns the formatted string to be sent to players.
---- * Can be redefined by mods if required, for things like colored names or messages.
---- * **Only** the first occurrence of each placeholder will be replaced.
---- @param name    string player name to send to
---- @param message string
-function core.format_chat_message(name, message) end
+--   see ./core_namespace/chat.lua
 
 -- Environment access:
 --- @param pos  MapPosition
---
+++ core_namespace/chat.lua
@@ -0,0 +1,21 @@
+--- @meta
+
+--- Chat
+--- Functions for sending messages to players and formatting chat output.
+
+--- @param text string
+function core.chat_send_all(text) end
+
+--- @param name string player name to send to
+--- @param text string
+function core.chat_send_player(name, text) end
+
+--- * Used by the server to format a chat message, based on the setting `chat_message_format`.
+---   Refer to the documentation of the setting for a list of valid placeholders.
+--- * Takes player name and message, and returns the formatted string to be sent to players.
+--- * Can be redefined by mods if required, for things like colored names or messages.
+--- * **Only** the first occurrence of each placeholder will be replaced.
+---
+--- @param name    string player name to send to
+--- @param message string
+function core.format_chat_message(name, message) end
